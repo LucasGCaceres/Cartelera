7662dd1e54a9a99fbeae922aedfbf426f3bfe6ca
Lucas Caceres <lucasgcaceres1@gmail.com>
2026-09-18 16:08:56 -0300

    correccion minima

--- backend/src/main/java/com/ezeiza/cartelera/config/SecurityConfig.java
+++ backend/src/main/java/com/ezeiza/cartelera/config/SecurityConfig.java
@@ -59,6 +59,7 @@ public class SecurityConfig {
 
                         .requestMatchers(
                                 "/",
+                                "/index.html",
                                 "/display",
                                 "/display/**",
                                 "/assets/**",
